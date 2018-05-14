cb929e157609280b07393c0aa66a2ab84c4d2143
Seeta Ram Yadav <sitaram@thoughtworks.com>
2018-05-14 11:06:45 +0530

    Learning clcik handler.

--- complete-guide/src/App.js
+++ complete-guide/src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    state = {
+        person: [
+            {name: 'firstName1', age: 21},
+            {name: 'firstName2', age: 23},
+            {name: 'firstName3', age: 25}
+        ]
+    };
+    clickSwitchHandler  = () => {
+        console.log('Switch clicked called :)!!!!!');
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello I am react App</h1>
+                <h1>This is really working</h1>
+                <button onClick={this.clickSwitchHandler}> Switch clicked </button>
+                <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+                <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
+                <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
+            </div>
+            // React.createElement('div',null,'h1','Hi I\'m a react App'); // This actually how react comples it.
+        );
+    }
 }
 
 export default App;
---
+++ complete-guide/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const person = (props) =>{
+    return (<p>I am {props.name}. My age is {props.age}</p>)
+}
+
+export default person;
